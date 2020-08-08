44d54574f08b81691c440d7d1131c9cba29dc007
takushi <sokoninoruna@gmail.com>
2020-08-08 23:55:42 +0900

    Test追加

---
+++ Project/Subroutine/BB001/BB001Test/OracleDbTest.cs
@@ -0,0 +1,24 @@
+﻿using BB001;
+using Xunit;
+
+namespace BB001Test
+{
+    public class BB001OracleDbTest
+    {
+        private readonly OracleDbSql _oracleDbSql;
+
+        public BB001OracleDbTest()
+        {
+            //準備
+            _oracleDbSql = new OracleDbSql();
+        }
+
+        [Fact]
+        public void BBPLAYSERSテーブルを全件検索できるSELECT文が書かれていること()
+        {
+
+            // 実行&検証
+            Assert.Equal("SELECT * FROM LEADERBOARD", _oracleDbSql.FindAllLeaderBoard);
+        }
+    }
+}
