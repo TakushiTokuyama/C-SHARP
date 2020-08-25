2b590c5506a5549d56e4758f2f7d5b1dbbb0148f
takushi <sokoninoruna@gmail.com>
2020-08-25 22:54:47 +0900

    テストダブル

---
+++ TestCode/TestDouble/TestDouble/UnitTest.cs
@@ -0,0 +1,76 @@
+﻿using Moq;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace TestDouble
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var mock = new Mock<IGreet>();
+
+            // プロパティ
+            var response = mock.SetupGet(x => x.Hello)
+                .Returns("こんにちは");
+
+            // メソッド
+            mock.Setup(x => x.Greet("こんにちは"))
+                .Returns("おはよう");
+
+            Assert.Equal("こんにちは", mock.Object.Hello);
+            Assert.Equal("おはよう", mock.Object.Greet("こんにちは"));
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var mock = new Mock<IGreet>();
+
+            string[] greets = { "おはよう", "こんにちは", "こんばんは" };
+
+            // メソッド
+            mock.Setup(x => x.Greet(It.Is<string>(s => greets.Contains(s))))
+                .Returns("あいさつ");
+
+            Assert.Equal("あいさつ", mock.Object.Greet("こんばんは"));
+
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var mock = new Mock<IGreet>();
+            // 全プロパティの定義
+            mock.SetupAllProperties();
+
+            var hello = mock.Object.Hello = "Hello";
+            var name = mock.Object.Name = "Taro";
+            var good = mock.Object.Good = "いいね！";
+
+            mock.Setup(x => x.Greet("あいさつ"))
+                .Returns(hello + name + good);
+
+            Assert.Equal("HelloTaroいいね！", mock.Object.Greet("あいさつ"));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var mock = new Mock<IGreet>();
+
+            // 複数返す
+            mock.SetupSequence(x => x.Greet("あいさつ"))
+                .Returns("おはよう")
+                .Returns("こんにちは")
+                .Returns("こんばんは");
+
+            Assert.Equal("おはよう", mock.Object.Greet("あいさつ"));
+            Assert.Equal("こんにちは", mock.Object.Greet("あいさつ"));
+            Assert.Equal("こんばんは", mock.Object.Greet("あいさつ"));
+        }
+    }
+}
