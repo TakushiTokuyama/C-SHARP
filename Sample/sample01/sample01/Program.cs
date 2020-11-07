11b607a598301dc0f1b522a47b6f6fcf7de93e9a
Takushi <sokoninoruna@gmail.com>
2020-11-07 09:22:30 +0900

    sample01追加

---
+++ Sample/sample01/sample01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace sample01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = string.Format("{0:yyyyMMdd}" + " " + "{1:hh:mm:ss}", DateTime.Today, DateTime.Now);
+
+            Console.WriteLine(log);
+
+            var filePath = Path.GetFileName(@"C:test\sample.txt");
+
+            Console.WriteLine(filePath);
+        }
+    }
+}
