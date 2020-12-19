b6a729cc2677f9093bb9d5fca4c67738da948e0a
TakushiTokuyama <55052628+TakushiTokuyama@users.noreply.github.com>
2020-12-19 22:19:14 +0900

    Thread && Task

---
+++ Practice/Practice/Practice1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ThreadSample thread = new ThreadSample();
+
+            //thread.Excute();
+
+            //TaskSample task = new TaskSample();
+
+            //task.Excute();
+        }
+    }
+}
---
+++ Practice/Practice/Practice1/ConsoleApp1/ConsoleApp1/ThreadSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public class ThreadSample
+    {
+        /// <summary>
+        /// Thread
+        /// </summary>
+        public void Excute()
+        {
+            Thread thread = new Thread(new ThreadStart(() =>
+            {
+                CountUp1();
+            }));
+
+            thread.Start();
+
+            CountUp2();
+        }
+
+        public void CountUp1() 
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(300);
+                Console.WriteLine("countUp1が呼ばれました。");
+            }
+        }
+
+        public void CountUp2() 
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("countUp2が呼ばれました。");
+            }
+        }
+    }
+}
