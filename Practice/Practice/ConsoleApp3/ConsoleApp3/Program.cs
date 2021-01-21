eccdbce4b0d74076615d76359da2e4bb75623824
takushi <sokoninoruna@gmail.com>
2021-01-21 23:39:14 +0900

    一旦push

---
+++ Practice/Practice/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void ProcessString(string s) 
+        {
+            if (s == null) 
+            {
+                throw new EmployeeListNotFounddException("aaaa");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                string s = null;
+                ProcessString(s);
+
+            }
+            catch (EmployeeListNotFounddException ex)
+            {
+                Console.WriteLine("{0} First exception caught.", ex.Message);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("{0} Second exception caught.", ex);
+            }
+
+        }
+    }
+}
---
+++ TestCode/TestCollection/TestCollection/CountService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCollection
+{
+    public class CountService
+    {
+        public int getCountService()
+        {
+            var syakeBentou = new FoodEntity("しゃけ弁当",450,1);
+
+            var yakinikuBentou = new FoodEntity("焼肉弁当", 500, 1);
+
+            var noriBentou = new FoodEntity("のり弁当", 400, 1);
+
+            var total = 0;
+
+            total += syakeBentou.foodPrice * syakeBentou.foodNum;
+            total += yakinikuBentou.foodPrice * yakinikuBentou.foodNum;
+            total += noriBentou.foodPrice * noriBentou.foodNum;
+
+            return total;
+        }
+
+        public string ParseLogLine(string input)
+        {
+            var sanitizedInput = TrimInput(input);
+            return sanitizedInput;
+        }
+
+        private string TrimInput(string input)
+        {
+            return input.Trim();
+        }
+    }
+}
