11966ed0c7ad4091264673d4383877a9a9fb4bed
takushi <sokoninoruna@gmail.com>
2021-02-13 00:05:54 +0900

    Deconstruction sample

---
+++ Practice/Practice/C_SHARP_7.0/C_SHARP_7.0/DeconstructionSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_SHARP_7._0
+{
+    class DeconstructionSample
+    {
+        public void Execute() 
+        {
+            // 分解　Deconstruction
+            var NameAndAge = (name: "Toku", age: 1122);
+            string name;
+            int age;
+            (name, age) = NameAndAge;
+
+            Console.WriteLine($"{name}And{age}");
+        }
+
+        public void Greeting(string name) 
+        {
+            var greets = (morning: "おはよう", noon: "こんにちは", night: "こんばんは");
+
+            var nowTime = int.Parse(DateTime.Now.ToString("HH"));
+
+            if (10 > nowTime && nowTime > 4)
+            {
+                Console.WriteLine(greets.morning);
+            }
+            else if (15 > nowTime && nowTime >= 10)
+            {
+                Console.WriteLine(greets.noon);
+            }
+            else 
+            {
+                Console.WriteLine(greets.night);
+            }
+        }
+    }
+}
