b426ce85d46b1efec2b034623635c13dd69a7b4b
takushi <sokoninoruna@gmail.com>
2021-02-28 10:35:14 +0900

    変更

---
+++ Practice/Practice/ConsoleApp3/ConsoleApp3/DateStr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class DateStr
+    {
+        public void Sample()
+        {
+            string dir = "D:test\\test2\\";
+
+            DateTime day = DateTime.Now;
+
+            string file = "\\test.txt";
+
+            string filePath = $@"{dir}{day.ToString("yyyyMMdd")}{file}";
+
+            Console.WriteLine(filePath);
+        }
+    }
+}
