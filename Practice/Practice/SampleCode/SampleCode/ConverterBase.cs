770ec0e221d36cccfae2ad9715946e53fdda8bc8
takushi <sokoninoruna@gmail.com>
2021-03-09 22:50:59 +0900

    追加

--- Practice/Practice/SampleCode/SampleCode/ConverterBase.cs
+++ Practice/Practice/SampleCode/SampleCode/ConverterBase.cs
@@ -1,29 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SampleCode
+﻿namespace SampleCode
 {
     public abstract class ConverterBase
     {
         /// <summary>
         /// 通貨の比率
         /// </summary>
-        public abstract double Ratio { get;}
+        protected abstract double Ratio { get; }
 
         /// <summary>
         /// 通貨の名前
         /// </summary>
-        public abstract string CurrencyName {get;}
+        public abstract string CurrencyName { get; }
 
         /// <summary>
         /// 円からの変換
         /// </summary>
         /// <param name="yen">日本円</param>
         /// <returns>円からの変換</returns>
-        public double FromYen(double yen) 
+        public double FromYen(double yen)
         {
             return yen / Ratio;
         }
@@ -33,11 +27,39 @@ namespace SampleCode
         /// </summary>
         /// <param name="yen">日本円</param>
         /// <returns>円への変換</returns>
-        public double ToYen(double yen) 
+        public double ToYen(double yen)
         {
-            return yen * Ratio; 
-        }   
+            return yen * Ratio;
+        }
     }
 
+    /// <summary>
+    /// 円
+    /// </summary>
+    public class YenConverter : ConverterBase
+    {
+        protected override double Ratio { get { return 1; } }
 
+        public override string CurrencyName { get { return "円"; } }
+    }
+
+    /// <summary>
+    /// ドル
+    /// </summary>
+    public class DollerConverter : ConverterBase
+    {
+        protected override double Ratio { get { return 0.009182; } }
+
+        public override string CurrencyName { get { return "ドル"; } }
+    }
+
+    /// <summary>
+    /// ユーロ
+    /// </summary>
+    public class EuroConverter : ConverterBase
+    {
+        protected override double Ratio { get { return 0.007700; } }
+
+        public override string CurrencyName { get { return "ユーロ"; } }
+    }
 }
---
+++ Practice/Practice/SampleCode/SampleCode/ExchangeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    /// <summary>
+    /// 為替
+    /// </summary>
+    public class ExchangeConverter
+    {
+        private ConverterBase From { get; set; }
+
+        private ConverterBase To { get; set; }
+
+        public ExchangeConverter(ConverterBase from, ConverterBase to) 
+        {
+            From = from;
+            To = to;
+        }
+
+        /// <summary>
+        /// コンバーター
+        /// </summary>
+        /// <returns>結果</returns>
+        public double Convert(double value) 
+        {
+            var yen = From.ToYen(value);
+            return To.FromYen(yen);
+        }
+    }
+}
